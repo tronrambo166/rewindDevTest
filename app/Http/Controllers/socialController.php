22b16e34e7b5af93b71c3dfb70fba682140c0cee
tronrambo166 <tronrambo166@gmail.com>
2023-07-18 19:46:12 +0600

    updated

--- app/Http/Controllers/socialController.php
+++ app/Http/Controllers/socialController.php
@@ -398,12 +398,13 @@ $insta_id =$collect->insta_pageid_of_fb;
 
         $response=curl_exec($curl); //dd($response);
         $response=json_decode($response,true);
-        if(isset($response['data'][0]['values'][1]['value'])){
+        if(isset($response['data'][0]['values'][1]['value']))
         $data['fans_by_city'] = $response['data'][0]['values'][1]['value'];
         else {
         if(isset($response['data'][0]['values'][0]['value'])){
         $data['fans_by_city'] = $response['data'][0]['values'][0]['value'];
         }
+    }
 
         //echo '<pre> taking = ';print_r($data['fans_by_city']);echo '<pre>'; exit;
 
