d11b8621948f5e2ce9cc19a8d135c4679f7e4eed
tronrambo166 <tronrambo166@gmail.com>
2023-07-20 16:09:00 +0600

    updated

--- app/Http/Controllers/socialController.php
+++ app/Http/Controllers/socialController.php
@@ -881,7 +881,6 @@ $insta_id =$collect->insta_pageid_of_fb;
         $video = json_encode($video);
 
         //header('location:https://test.muziqyrewind.com/tiktok_social?data='.$video.'&user='.$user);
-       $video ='test'; $user = 'test';
        $test = '<script> window.location.href="https://test.muziqyrewind.com/tiktok_social?data='.$video.'&user='.$user.'" </script>';
         //echo '<pre>';print_r($response);echo '<pre>';exit;
        echo $test;
